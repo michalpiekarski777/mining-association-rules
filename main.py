ef06a800890e8684cda5bf5e734f1f1c1a536619
Michal Piekarski <michal.piekarski@profil-software.com>
2024-04-25 11:28:26 +0200

    MAR: Start parsing input arguments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import sys
 from pathlib import Path
@@ -19,6 +20,7 @@ from src.mining_association_rules.apriori_df.interest_measures.rule_interest_fun
 )
 from src.mining_association_rules.apriori_df.interest_measures.support import Support
 from src.mining_association_rules.apriori_list.apriori.apriori import ListRuleGenerator
+from src.mining_association_rules.common.utils.enums import RunnerType
 from src.mining_association_rules.common.utils.read_csv import read_transactions_shop
 from src.mining_association_rules.common.utils.runners import run
 
@@ -26,32 +28,58 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-def main():
-    runner = sys.argv[1] if len(sys.argv) > 1 else "default"
-    if runner == "default":
-        source = "survey.parquet"
-        df = pd.read_parquet(Path(ROOT_DIR) / "sources" / source)
-        rule_gen = DataFrameRuleGenerator(
-            source=source,
-            itemset_measure=Support,
-            rule_measures=[
-                AntiSupport,
-                Confidence,
-                Conviction,
-                DependencyFactor,
-                GainFunction,
-                Lift,
-                RuleInterestFunction,
-            ],
-        )
-        kwargs = dict(transactions=df)
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="Mining association rules",
+        description="Generator of association rules from your dataset",
+    )
+    parser.add_argument(
+        "-f",
+        "--file",
+        required=True,
+        type=str,
+        help="Name of Parquet file placed in sources directory",
+    )
+    parser.add_argument("-r", "--runner", default="df", choices=["df", "list"])
+    return parser.parse_args()
 
+
+def prepare_df_gen(source: str) -> tuple[DataFrameRuleGenerator, dict]:
+    df = pd.read_parquet(Path(ROOT_DIR) / "sources" / source)
+    rule_gen = DataFrameRuleGenerator(
+        source=source,
+        itemset_measure=Support,
+        rule_measures=[
+            AntiSupport,
+            Confidence,
+            Conviction,
+            DependencyFactor,
+            GainFunction,
+            Lift,
+            RuleInterestFunction,
+        ],
+    )
+    kwargs = dict(transactions=df)
+    return rule_gen, kwargs
+
+
+def prepare_list_gen(source: str) -> tuple[ListRuleGenerator, dict]:
+    path = Path(ROOT_DIR) / "sources" / source
+    elements, transactions = read_transactions_shop(path)
+    rule_gen = ListRuleGenerator(source=source)
+    kwargs = dict(transactions=transactions, elements=elements)
+    return rule_gen, kwargs
+
+
+def main():
+    args = parse_args()
+
+    if args.runner == RunnerType.DATAFRAME.value:
+        rule_gen, kwargs = prepare_df_gen(args.file)
+    elif args.runner == RunnerType.LIST.value:
+        rule_gen, kwargs = prepare_list_gen(args.file)
     else:
-        source = "shop.csv"
-        path = Path(ROOT_DIR) / "sources" / source
-        elements, transactions = read_transactions_shop(path)
-        rule_gen = ListRuleGenerator(source=source)
-        kwargs = dict(transactions=transactions, elements=elements)
+        sys.exit("Invalid runner")
 
     run(rule_gen, kwargs)
 
--- src/mining_association_rules/common/utils/enums.py
+++ src/mining_association_rules/common/utils/enums.py
@@ -4,3 +4,8 @@ from enum import StrEnum
 class FileFormat(StrEnum):
     PARQUET = "parquet"
     CSV = "csv"
+
+
+class RunnerType(StrEnum):
+    DATAFRAME = "df"
+    LIST = "list"
