0603cc2ac0c717a151ea5c533312e468a7271623
michalpiekarski777 <michal.piekarski9@gmail.com>
2025-05-27 13:50:07 +0200

    MAR: Remove master thesis related files

--- example_usage.py
+++ example_usage.py
@@ -4,15 +4,15 @@ import pandas as pd
 
 from config import ROOT_DIR
 from src.mar.apriori_df.apriori import DataFrameRuleGenerator
-from src.mar.apriori_df.interest_measures.batch_confidence import BatchConfidence
-from src.mar.apriori_df.interest_measures.batch_support import BatchSupport
+from src.mar.apriori_df.interest_measures import Confidence
+from src.mar.apriori_df.interest_measures import Support
 
 # read dataset from file put in sources catalog
 path = Path(ROOT_DIR) / "sources" / "survey.parquet"
 df = pd.read_parquet(path)
 # specify measures used during the processing with its thresholds
-itemset_measures = {BatchSupport: 0.6}
-rule_measures = {BatchConfidence: 0.6}
+itemset_measures = {Support: 0.6}
+rule_measures = {Confidence: 0.6}
 # run algorithm
 with DataFrameRuleGenerator(itemset_measures=itemset_measures, rule_measures=rule_measures) as rule_gen:
     rule_gen.generate_strong_association_rules(transactions=df)
--- example_usage2.py
+++ example_usage2.py
@@ -1,6 +1,6 @@
 from src.mar.apriori_df.apriori import DataFrameRuleGenerator
-from src.mar.apriori_df.interest_measures import BatchConfidence
-from src.mar.apriori_df.interest_measures import BatchSupport
+from src.mar.apriori_df.interest_measures import Confidence
+from src.mar.apriori_df.interest_measures import Support
 from src.mar.common.utils.csv_to_df import convert_dataset_to_dataframe
 
 # convert your dataset to required format
@@ -14,8 +14,8 @@ transactions = [
 ]
 df = convert_dataset_to_dataframe(elements, transactions)
 # specify measures used during the processing with its thresholds
-itemset_measures = {BatchSupport: 0.4}
-rule_measures = {BatchConfidence: 0.2}
+itemset_measures = {Support: 0.4}
+rule_measures = {Confidence: 0.2}
 # run algorithm
 rule_gen = DataFrameRuleGenerator(itemset_measures=itemset_measures, rule_measures=rule_measures)
 rules = rule_gen.generate_strong_association_rules(transactions=df)
